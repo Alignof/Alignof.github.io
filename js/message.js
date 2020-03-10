a4ef3f2632da122eb8cc02a31da24a5ceb5727b3
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-03-11 01:52:24 +0900

    links valiable

--- js/message.js
+++ js/message.js
@@ -9,10 +9,9 @@ let tail='<span class="time">['+Hour+':'+Min+':'+Sec+']</span></div>';
 
 let wait='<span class="terminal">wait...</span>'
 let hello=head+'<span class="terminal"><del>Hello! Hackers!<ins>0.7</ins></del>cat welcome</span>'+tail;
-let ls=head+'<span class="terminal">ls</span>'+tail;
-let whoami=head+'<span class="terminal">whoami</span>'+tail;
 
-let welcome_message='<pre id="AA" lang="en"><font color="yellow">'+
+let welcome_message=
+'<pre id="AA" lang="en"><font color="yellow">'+
 " __          __  _                                   _                             <br>"+
 " \\ \\        / / | |                                 | |                            <br>"+
 "  \\ \\  /\\  / /__| | ___   ___ ___  _ __ ___   ___   | |_ ___                       <br>"+
@@ -28,8 +27,30 @@ let welcome_message='<pre id="AA" lang="en"><font color="yellow">'+
 "                                  |___/   |_|                                      </font></pre>"+
 "<pre>                                                             <font size=4 color='red'>2020.03.10 create by Takana Norimasa</font></pre>";
 
-let whoami_result='<pre>';
+let about=
+'<div id="about">'+
+"<pre>Name: Takana Norimasa</pre>"+
+"<pre>Age: 18</pre>"+
+"<pre>Nationality: Japan</pre>"+
+"<pre>Affiliation: National Institute of Technology, Kisarazu College</pre>"+
+"<pre>Language: C,C++,Perl,Python,vimscript</pre>"+
+"<pre>Interests: cyber security,malware analysis,compiler,handmade cpu,I/O device,ESP32,etc...</pre>"+
+"</div>";
 
+let links=
+'<div id="about">'+
+'<pre><i class="far fa-envelope"></i> official: j17423@kisarazu.kosen-ac.jp</pre>'+
+'<pre><i class="far fa-envelope"></i> sub: neuromancer_wg@outlook.jp</pre>'+
+'<pre><font color="cyan"><i class="fab fa-twitter-square"></i></font> [main/technology]:<a href="https://twitter.com/Seigenkousya_"> @seigenkousya_</a></pre>'+
+'<pre><font color="cyan"><i class="fab fa-twitter-square"></i></font> [sub/tech&anime]:<a href="https://twitter.com/Seigenkousya"> @seigenkousya</a></pre>'+
+'<pre><i class="fab fa-github-square"></i> [main/works]:<a href="https://github.com/Takana-Norimasa"> Takana-Norimasa</a></pre>'+
+'<pre><i class="fab fa-github-square"></i> [sub/hobby]:<a href="https://github.com/Seigenkousya"> Seigenkousya</a></pre>'+
+'<pre><span class="fa-stack" style="color:#4cb10d;font-size:50%"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-search fa-stack-1x fa-inverse fa-2x"></i></span><a href="https://qiita.com/Seigenkousya"> Seigenkousya</a></pre>'+
+'</div>';
+
+function cmd(command){
+	return head+'<span class="terminal">'+command+'</span>'+tail;
+}
 
 function time_update(){
 	now=new Date();
@@ -55,8 +76,12 @@ async function main_stream(){
 	await message(wait,0);
 	await message(hello,0);
 	await message(welcome_message,0);
-	await message(whoami,0);
-	await message(ls,0);
+	await message(cmd("whoami"),0);
+	await message(about,0);
+	await message(cmd("ls"),0);
+	await message(cmd("clear"),0);
+	await message(cmd("cat links"),0);
+	await message(links,0);
 }
 
 console.log("start");
