f0c6b8dfc5b1ef9f5f23b336f70ffbc4c71082aa
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-04-22 23:52:27 +0900

    sync parfectry!!!!!!!!!!!!!!!

--- js/button.js
+++ js/button.js
@@ -1,36 +1,36 @@
 /*------------------root--------------------------*/
 async function about_me(){
-	await message(cmd("cat about_me"),2000);
-	await message(about,2000);
-	await message(cmd("ls -a"),2000);
-	await message(ls_root,0);
+	await typing(cmd("cat about_me"));
+	await message(about);
+	await typing(cmd("ls -a"));
+	await message(ls_root);
 }
 
 async function back_to_root(path){
-	await message(cmd_path(path,"cd .."),2000);
-	await message(cmd("ls -a"),2000);
-	await message(ls_root,0);
+	await typing(cmd_path(path,"cd .."));
+	await typing(cmd("ls -a"));
+	await message(ls_root);
 }
 
 async function contact_and_links(){
-	await message(cmd("cat contact_and_links"),2000);
-	await message(links,2000);
-	await message(cmd("ls -a"),2000);
-	await message(ls_root,0);
+	await typing(cmd("cat contact_and_links"));
+	await message(links);
+	await typing(cmd("ls -a"));
+	await message(ls_root);
 }
 
 async function welcome(){
-	await message(cmd("cat welcome"),2000);
-	await message(welcome_message,2000);
-	await message(cmd("ls -a"),2000);
-	await message(ls_root,0);
+	await typing(cmd("cat welcome"));
+	await message(welcome_message);
+	await typing(cmd("ls -a"));
+	await message(ls_root);
 }
 
 async function tree(){
-	await message(cmd("tree -C"),2000);
-	await message(tree_result,2000);
-	await message(cmd("ls -a"),2000);
-	await message(ls_root,0);
+	await typing(cmd("tree -C"));
+	await message(tree_result);
+	await typing(cmd("ls -a"));
+	await message(ls_root);
 }
 
 
@@ -38,89 +38,89 @@ async function tree(){
 
 /*------------------Project--------------------------*/
 async function project(){
-	await message(cmd("cd Project/"),2000);
-	await message(cmd_path("Project","ls -a"),2000);
-	await message(ls_project,0);
+	await typing(cmd("cd Project/"));
+	await typing(cmd_path("Project","ls -a"));
+	await message(ls_project);
 }
 
 async function IO_Unit(){
-	await message(cmd_path("Project","cat I/O Unit"),2000);
-	await message(page_IO_Unit,0);
+	await typing(cmd_path("Project","cat I/O Unit"));
+	await message(page_IO_Unit);
 	await reload_iframely();
-	await message(cmd_path("Project","ls -a"),2000);
-	await message(ls_project,0);
+	await typing(cmd_path("Project","ls -a"));
+	await message(ls_project);
 }
 
 async function timer_stoper(){
-	await message(cmd_path("Project","cat Juggernaut"),2000);
-	await message(page_jaggernaut,0);
+	await typing(cmd_path("Project","cat Juggernaut"));
+	await message(page_jaggernaut);
 	await reload_iframely();
-	await message(cmd_path("Project","ls -a"),2000);
-	await message(ls_project,0);
+	await typing(cmd_path("Project","ls -a"));
+	await message(ls_project);
 }
 
 async function repojitories(){
-	await message(cmd_path("Project","cat Repositories"),2000);
-	await message(page_repositories,0);
+	await typing(cmd_path("Project","cat Repositories"));
+	await message(page_repositories);
 	await reload_contribution();
 	await reload_iframely();
-	await message(cmd_path("Project","ls -a"),2000);
-	await message(ls_project,0);
+	await typing(cmd_path("Project","ls -a"));
+	await message(ls_project);
 }
 
 
 
 /*------------------Activities--------------------------*/
 async function activities(){
-	await message(cmd("cd Activities/"),2000);
-	await message(cmd_path("Activities","ls -a"),2000);
-	await message(ls_activities,0);
+	await typing(cmd("cd Activities/"));
+	await typing(cmd_path("Activities","ls -a"));
+	await message(ls_activities);
 }
 
 async function chronology(){
-	await message(cmd_path("Activities","./chronology"),2000);
-	await message(cmd_path("Activities","ls -a"),2000);
-	await message(ls_activities,0);
+	await typing(cmd_path("Activities","./chronology"));
+	await typing(cmd_path("Activities","ls -a"));
+	await message(ls_activities);
 }
 
 async function sechack365(){
-	await message(cmd_path("Activities","cat SecHack365"),2000);
-	await message(page_sechack365,0);
+	await typing(cmd_path("Activities","cat SecHack365"));
+	await message(page_sechack365);
 	await reload_iframely();
-	await message(cmd_path("Activities","ls -a"),2000);
-	await message(ls_activities,0);
+	await typing(cmd_path("Activities","ls -a"));
+	await message(ls_activities);
 }
 
 async function guardian(){
-	await message(cmd_path("Activities","cat Guardian Project"),2300);
-	await message(page_gurdian_project,0);
+	await typing(cmd_path("Activities","cat Guardian Project"));
+	await message(page_gurdian_project);
 	await reload_iframely();
-	await message(cmd_path("Activities","ls -a"),2000);
-	await message(ls_activities,0);
+	await typing(cmd_path("Activities","ls -a"));
+	await message(ls_activities);
 }
 
 async function IoT_Hackathon(){
-	await message(cmd_path("Activities","cat LoRaWAN IoT Hackathon"),2500);
-	await message(page_iot_hackathon,0);
+	await typing(cmd_path("Activities","cat LoRaWAN IoT Hackathon"));
+	await message(page_iot_hackathon);
 	await reload_iframely();
-	await message(cmd_path("Activities","ls -a"),2000);
-	await message(ls_activities,0);
+	await typing(cmd_path("Activities","ls -a"));
+	await message(ls_activities);
 }
 
 async function atcoder(){
-	await message(cmd_path("Activities","cat Atcoder"),2000);
-	await message(page_atcoder,0);
+	await typing(cmd_path("Activities","cat Atcoder"));
+	await message(page_atcoder);
 	await reload_iframely();
-	await message(cmd_path("Activities","ls -a"),2000);
-	await message(ls_activities,0);
+	await typing(cmd_path("Activities","ls -a"));
+	await message(ls_activities);
 }
 
 async function CTF(){
-	await message(cmd_path("Activities","cat CTF"),2000);
-	await message(page_ctf,0);
+	await typing(cmd_path("Activities","cat CTF"));
+	await message(page_ctf);
 	await reload_iframely();
-	await message(cmd_path("Activities","ls -a"),2000);
-	await message(ls_activities,0);
+	await typing(cmd_path("Activities","ls -a"));
+	await message(ls_activities);
 }
 
 
@@ -128,32 +128,32 @@ async function CTF(){
 
 /*------------------Hobby--------------------------*/
 async function hobby(){
-	await message(cmd("cd Hobby/"),2000);
-	await message(cmd_path("Hobby","ls -a"),2000);
-	await message(ls_hobby,0);
+	await typing(cmd("cd Hobby/"));
+	await typing(cmd_path("Hobby","ls -a"));
+	await message(ls_hobby);
 }
 
 async function comic(){
-	await message(cmd_path("Hobby","cat comic"),2000);
-	await message(page_comic,0);
+	await typing(cmd_path("Hobby","cat comic"));
+	await message(page_comic);
 	await reload_iframely();
-	await message(cmd_path("Hobby","ls -a"),2000);
-	await message(ls_hobby,0);
+	await typing(cmd_path("Hobby","ls -a"));
+	await message(ls_hobby);
 }
 
 async function anime(){
-	await message(cmd_path("Hobby","cat anime"),2000);
-	await message(page_anime,0);
-	await message(cmd_path("Hobby","ls -a"),2000);
-	await message(ls_hobby,0);
+	await typing(cmd_path("Hobby","cat anime"));
+	await message(page_anime);
+	await typing(cmd_path("Hobby","ls -a"));
+	await message(ls_hobby);
 }
 
 async function motor_sports(){
-	await message(cmd_path("Hobby","motor sports"),2200);
-	await message(page_motor_sports,0);
+	await typing(cmd_path("Hobby","motor sports"));
+	await message(page_motor_sports);
 	await reload_iframely();
-	await message(cmd_path("Hobby","ls -a"),2000);
-	await message(ls_hobby,0);
+	await typing(cmd_path("Hobby","ls -a"));
+	await message(ls_hobby);
 }
 
 
@@ -161,56 +161,56 @@ async function motor_sports(){
 
 /*------------------Seigenkousya--------------------------*/
 async function SIN_dir(){
-	await message(cmd_path("Hobby/","cd Seigenkousya/"),2300);
-	await message(cmd_path("Hobby/Seigenkousya","ls -a"),2000);
-	await message(ls_seigenkousya,0);
+	await typing(cmd_path("Hobby/","cd Seigenkousya/"));
+	await typing(cmd_path("Hobby/Seigenkousya","ls -a"));
+	await message(ls_seigenkousya);
 }
 
 async function about_SIN(){
-	await message(cmd_path("Hobby/Seigenkousya","cat about_us"),2000);
-	await message(page_about_SIN,0);
+	await typing(cmd_path("Hobby/Seigenkousya","cat about_us"));
+	await message(page_about_SIN);
 	await reload_iframely();
 	await reload_contribution();
-	await message(cmd_path("Hobby/Seigenkousya","ls -a"),2000);
-	await message(ls_seigenkousya,0);
+	await typing(cmd_path("Hobby/Seigenkousya","ls -a"));
+	await message(ls_seigenkousya);
 }
 
 async function blog(){
-	await message(cmd_path("Hobby/Seigenkousya","cat homepage"),2000);
-	await message(page_homepage,0);
+	await typing(cmd_path("Hobby/Seigenkousya","cat homepage"));
+	await message(page_homepage);
 	await reload_iframely();
-	await message(cmd_path("Hobby/Seigenkousya","ls -a"),2000);
-	await message(ls_seigenkousya,0);
+	await typing(cmd_path("Hobby/Seigenkousya","ls -a"));
+	await message(ls_seigenkousya);
 }
 
 async function SIN_link(){
-	await message(cmd_path("Hobby/Seigenkousya","browther Seigenkousya.html"),2800);
+	await typing(cmd_path("Hobby/Seigenkousya","browther Seigenkousya.html"));
 	// open new tab
 	window.open('https://seigenkousya.github.io/', '_blank');
-	await message(cmd_path("Hobby/Seigenkousya","ls -a"),2000);
-	await message(ls_seigenkousya,0);
+	await typing(cmd_path("Hobby/Seigenkousya","ls -a"));
+	await message(ls_seigenkousya);
 }
 
 async function scraper(){
-	await message(cmd_path("Hobby/Seigenkousya","cat kirara_scraper"),2500);
-	await message(page_kirara_scraper,0);
+	await typing(cmd_path("Hobby/Seigenkousya","cat kirara_scraper"));
+	await message(page_kirara_scraper);
 	await reload_iframely();
-	await message(cmd_path("Hobby/Seigenkousya","ls -a"),2000);
-	await message(ls_seigenkousya,0);
+	await typing(cmd_path("Hobby/Seigenkousya","ls -a"));
+	await message(ls_seigenkousya);
 }
 
 async function kirara_data(){
-	await message(cmd_path("Hobby/Seigenkousya","cat KiraraData.com"),2500);
-	await message(page_kirara_database,0);
+	await typing(cmd_path("Hobby/Seigenkousya","cat KiraraData.com"));
+	await message(page_kirara_database);
 	await reload_iframely();
-	await message(cmd_path("Hobby/Seigenkousya","ls -a"),2000);
-	await message(ls_seigenkousya,0);
+	await typing(cmd_path("Hobby/Seigenkousya","ls -a"));
+	await message(ls_seigenkousya);
 }
 
 async function back_to_hobby(){
-	await message(cmd_path("Hobby/Seigenkousya","cd ..",2000));
-	await message(cmd_path("Hobby","ls -a"),2000);
-	await message(ls_hobby,0);
+	await typing(cmd_path("Hobby/Seigenkousya","cd ..",2000));
+	await typing(cmd_path("Hobby","ls -a"));
+	await message(ls_hobby);
 }
 
 
--- js/message.js
+++ js/message.js
@@ -11,8 +11,7 @@ let tail='<span class="time">['+Hour+':'+Min+':'+Sec+']</span></div>';
 let wait='<span class="terminal_0"><font color="#FFFFFF">wait...</font></span>'
 let hello=head+'<span class="terminal_1"><font color="#FFFFFF"><del>Hello! Hackers!<ins>0.7</ins></del>cat welcome</font></span>'+tail;
 
-
-function tjs(num){
+const tjs=function(num,res){
 	$((".terminal_"+num)).t({
 		delay:1,		// start delay in seconds [default:0]
 		speed:40,               // typing speed (ms) [default:50]
@@ -31,9 +30,39 @@ function tjs(num){
 		// typing callback
 		typing:function(elm,chr_or_elm,left,total){},
 		// finished callback
-		fin:function(elm){$('terminal_'+num).find('.t-caret').css({opacity:0});}          
+		fin:function(elm){
+			$('terminal_'+num).find('.t-caret').css({opacity:0});
+			console.log("tjs ended.");
+			counter++;
+			console.log(res());
+		}       
 	})
 }
+
+/*
+function tjs(num){
+		$((".terminal_"+num)).t({
+			delay:1,		// start delay in seconds [default:0]
+			speed:40,               // typing speed (ms) [default:50]
+			speed_vary:false,	// 'human like' speed variation [default:false]
+			mistype:false,		// mistype rate: 1:N per char [default:false]
+			locale:'en',            // keyboard layout (to fit mistype); supported: 'en' (english) or 'de' (german) [default:'en']
+			caret:true,		// caret; can be HTML; associated CSS class:.t-caret; default (TRUE): ▎ (\u258e)
+			blink:true,             // blink-interval in ms; if TRUE, speed*3  [default:true]
+			blink_perm:false,       // permanent blinking? if FALSE, caret blinks only on delay/pause/finish [default:false]
+			repeat:0,               // repeat typing: if TRUE, infinite or N times [default:0]
+			tag:'span',             // wrapper tag (.t-container, .t-caret) [default:'span']
+			pause_on_click:false,   // pauses/continues typing on click/tap (elm) [default:false]
+
+			// init callback (ready-to-type)
+			init:function(elm){},        
+			// typing callback
+			typing:function(elm,chr_or_elm,left,total){},
+			// finished callback
+			fin:function(elm){$('terminal_'+num).find('.t-caret').css({opacity:0});}          
+		})
+}
+*/
 
 function cmd(command){
 	return head+'<span class="'+"terminal_"+counter+'">'+'<font color="#FFFFFF">'+command+'</font>'+'</span>'+tail;
@@ -52,27 +81,35 @@ function time_update(){
 	tail='<span class="time">['+Hour+':'+Min+':'+Sec+']</span></div>';
 }
 
-function message(text,delay){
+function message(text){
 	const p=new Promise(async(resolve,reject) => {
+		scroll_bottom();
+		await new Promise(r=>setTimeout(r, 300));
 		time_update();
 		terget.insertAdjacentHTML('beforeend',text);
-		tjs(counter);
-		scroll_bottom();
-		
-		await new Promise(r=>setTimeout(r, delay));
-		counter++;
 		resolve();
 	});
 	return p;
 }
 
+function typing(text){
+	const p=new Promise(async(resolve,reject) => {
+		scroll_bottom();
+		await new Promise(r=>setTimeout(r, 100));
+		time_update();
+		terget.insertAdjacentHTML('beforeend',text);
+		await tjs(counter,resolve);
+	});
+	return p;
+}
+
 function scroll_bottom(){
-		const targetElement=document.getElementById('blank');
-		const rectTop=targetElement.getBoundingClientRect().top
-		const offsetTop=window.pageYOffset
-		const buffer=50
-		const top=rectTop + offsetTop - buffer
-		window.scrollTo({top,behavior: "smooth"});
+	const targetElement=document.getElementById('blank');
+	const rectTop=targetElement.getBoundingClientRect().top
+	const offsetTop=window.pageYOffset
+	const buffer=50
+	const top=rectTop + offsetTop - buffer
+	window.scrollTo({top,behavior: "smooth"});
 }
 
 function reload_iframely(){
@@ -88,13 +125,13 @@ function reload_contribution(){
 }
 
 async function main_stream(){
-	await message(wait,2800);
-	await message(hello,4600);
-	await message(welcome_message,0);
-	await message(cmd("whoami"),2000);
-	await message(about,0);
-	await message(cmd("ls -a"),2000);
-	await message(ls_root,0);
+	await typing(wait);
+	await typing(hello);
+	await message(welcome_message);
+	await typing(cmd("whoami"));
+	await message(about);
+	await typing(cmd("ls -a"));
+	await message(ls_root);
 }
 
 console.log("start");
